43da34bb709f8f7054b0f9270e9e991991d90edb
zhoulihua <lihua@eastmoney.com>
2018-09-06 16:15:06 +0800

    add new file

---
+++ LZBarrage/LZBarrageDescriptor.h
@@ -0,0 +1,13 @@
+//
+//  LZBarrageDescriptor.h
+//  LZBarrage
+//
+//  Created by zlh on 18/9/6.
+//  Copyright © 2018年 zlh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LZBarrageDescriptor : NSObject
+
+@end
---
+++ LZBarrage/LZBarrageDescriptor.m
@@ -0,0 +1,16 @@
+//
+//  LZBarrageDescriptor.m
+//  LZBarrage
+//
+//  Created by zlh on 18/9/6.
+//  Copyright © 2018年 zlh. All rights reserved.
+//
+
+#import "LZBarrageDescriptor.h"
+
+@interface LZBarrageDescriptor()
+@end
+
+@implementation LZBarrageDescriptor
+
+@end
